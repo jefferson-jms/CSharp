d19bc1d37d6a5c0f1977c0856be02366b3457849
jefferson-jms <83091992+jefferson-jms@users.noreply.github.com>
2021-11-25 16:00:08 -0300

    estrutura de repetição

---
+++ estrutura de repetição/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EstruturasRepeticao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+for(int i = 1;i <= 10; i+=2)
+    Console.WriteLine($"o valor de i é: {i}");
+
+        }
+    }
+}
